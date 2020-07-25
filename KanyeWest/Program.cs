5d252de1f5db723ea5853b53a9c1a7dc4853027d
johndward01 <johndward01@yahoo.com>
2020-07-25 16:10:46 -0500

    Kanye West / Ron Swanson API call exercise completed

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeWest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest";
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"Kanye: {kanyeQuote}");
+            Console.WriteLine();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            for (int i = 0; i < 5; i++)
+            {
+                var ronResponse = client.GetStringAsync(ronURL).Result;
+                var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+                kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+                kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+                Console.WriteLine($"Ron: {ronQuote}\n Kanye: {kanyeQuote}");
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
